3cacdbcaeb54dd2eae53d10edce6851802d0327f
Elias Eskelinen <elias.eskelinen@pm.me>
2022-07-13 01:02:18 +0300

    improve editor guidance

--- src/routes/editor.svelte
+++ src/routes/editor.svelte
@@ -206,7 +206,7 @@ import Rule from "$lib/components/Rule.svelte";
                 </div>
             </div>
         {:else}
-            <h1>Please select a tile from the left</h1>
+            <h1>Please select a tile by clicking one</h1>
         {/if}
     </div>
 </main>
